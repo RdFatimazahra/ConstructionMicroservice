5573a8e0e5c7f189ad033e39b560137fa63dc863
RdFatimazahra <fatimaezahraradi@gmail.com>
2024-08-27 15:14:29 +0100

    Create tables in projectService

---
+++ ProjetService/src/main/resources/db/migration/V1__Create_tables.sql
@@ -0,0 +1,8 @@
+CREATE TABLE projet (
+                        id_projet SERIAL PRIMARY KEY,
+                        nom_projet VARCHAR(255) NOT NULL,
+                        description TEXT,
+                        date_debut DATE,
+                        date_fin DATE,
+                        budget DECIMAL(10, 2)
+);
