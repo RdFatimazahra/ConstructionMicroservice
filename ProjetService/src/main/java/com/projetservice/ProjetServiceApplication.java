d9e0845ec61c5e4ba9f7630a8e4370d665a17fec
RdFatimazahra <fatimaezahraradi@gmail.com>
2024-08-28 10:53:23 +0100

    Create  ProjetController class

--- ProjetService/src/main/java/com/projetservice/ProjetServiceApplication.java
+++ ProjetService/src/main/java/com/projetservice/ProjetServiceApplication.java
@@ -2,8 +2,10 @@ package com.projetservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ProjetServiceApplication {
 
     public static void main(String[] args) {
