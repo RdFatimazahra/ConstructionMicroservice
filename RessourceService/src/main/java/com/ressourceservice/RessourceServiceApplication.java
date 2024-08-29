8b019cb5637377c3dcfb41dc90208d413f4f4ebb
RdFatimazahra <fatimaezahraradi@gmail.com>
2024-08-29 12:44:55 +0100

    Create model, DTO and mapper class for RessourceService

--- RessourceService/src/main/java/com/ressourceservice/RessourceServiceApplication.java
+++ RessourceService/src/main/java/com/ressourceservice/RessourceServiceApplication.java
@@ -1,9 +1,12 @@
 package com.ressourceservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class RessourceServiceApplication {
 
     public static void main(String[] args) {
--- RessourceService/src/main/java/com/ressourceservice/mapper/RessourceMapper.java
+++ RessourceService/src/main/java/com/ressourceservice/mapper/RessourceMapper.java
@@ -1,4 +1,15 @@
 package com.ressourceservice.mapper;
 
+
+import com.ressourceservice.Dto.RessourceDto;
+import com.ressourceservice.model.Ressource;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
 public interface RessourceMapper {
+    RessourceMapper INSTANCE = Mappers.getMapper(RessourceMapper.class);
+    
+    RessourceDto ressourceToRessourceDto(Ressource ressource);
+    Ressource ressourceDtoToRessource(RessourceDto ressourceDto);
 }
